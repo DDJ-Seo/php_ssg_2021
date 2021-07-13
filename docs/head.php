bfb8a639c1516850a80b0baea2a8891790eac84f
DDJ-Seo <dongjoo0714@gmail.com>
2021-07-13 18:44:35 +0900

    php 연습

--- docs/head.php
+++ docs/head.php
@@ -5,7 +5,6 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>디자이너 서동주</title>
-  
   <!-- 제이쿼리 불러오기 -->
   <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
   <!-- 테일윈드 -->
@@ -15,9 +14,28 @@
   <!-- 공통 CSS -->
   <link rel="stylesheet" href="common.css">
   <!-- 공통 JS -->
-  <link rel="stylesheet" href="common.js">
+  <script src="common.js"></script>
 </head>
 <body>
-  
-</body>
-</html>
+  <div class="site-wrap">
+    <header class="h-12 bg-red-500 top-bar">
+      <div class="container flex h-full mx-auto">
+        <a href="./" class="flex items-center px-4 logo">
+          로고
+        </a>
+        <div class="flex-grow"></div>
+        <ul class="flex top-bar__menu-box-1">
+          <li>
+            <a href="./" class="flex items-center h-full px-4">
+              홈
+            </a>
+          </li>
+          <li>
+            <a href="./portfolio.php" class="flex items-center h-full px-4">
+              포트폴리오
+            </a>
+          </li>
+        </ul>
+      </div>
+    </header>
+    <main>
